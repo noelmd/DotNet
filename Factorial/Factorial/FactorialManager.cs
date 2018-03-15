ca964920b03560202e462c3af2acd024ab04dc3c
Noel Mamoghli <noel.nmd@gmail.com>
2018-03-15 17:05:03 +0100

    Factorial Commit
    Upload Factorial Project

---
+++ Factorial/Factorial/FactorialManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factorial
+{
+    public static class FactorialManager
+    {
+        public static long Factorial(long numero)
+        {
+            long resultado;
+
+            if (numero < 0)
+                throw new Exception("El número debe ser positivo");
+            else if (numero == 0)
+            {
+                resultado =  1;
+            }
+            else
+            {
+                resultado = numero * Factorial(numero - 1);
+            }
+
+            return resultado;
+        }
+
+        public static long FactorialIterativo(long numero)
+        {
+            long resultado = 1;
+
+            if (numero < 0)
+                throw new Exception("El parámetro debe ser positivo");
+
+            for (long i = 1; i <= numero; i++)
+            {
+                resultado *= i;
+            }
+
+            return resultado;
+        }
+    }
+}
---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            long numero;
+
+            do
+            {
+                Console.WriteLine("Escribe un número cuyo factorial quieras conocer...");
+            }
+            while (!long.TryParse(Console.ReadLine(), out numero));
+
+            numero = Math.Abs(numero);
+
+            Console.WriteLine("El factorial de {0} es {1}", numero, FactorialManager.Factorial(numero));
+
+            Console.ReadKey();
+        }
+    }
+}
