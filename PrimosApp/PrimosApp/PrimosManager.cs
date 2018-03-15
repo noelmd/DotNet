25eb9cbdc0e00b2e6c232fb1497a2fdc66468af4
Noel Mamoghli <noel.nmd@gmail.com>
2018-03-15 17:03:16 +0100

    First Commit
    First Commit

---
+++ PrimosApp/PrimosApp/PrimosManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimosApp
+{
+    class PrimosManager
+    {
+        public static bool isPrimo(int numero)
+        {
+            bool primo = true;
+
+            if (!checkCasosEspeciales(numero))
+            {
+                int numeroInicial = numero;
+
+                while ((numero > 2) && (primo == true))
+                {
+                    numero--;
+
+                    if (numeroInicial % numero == 0)
+                    {
+                        primo = false;
+                    }
+                }
+            }
+            else
+            {
+                primo = false;
+            }
+            
+            return primo;
+        }
+
+        private static bool checkCasosEspeciales (int numero)
+        {
+            bool resuelto = false;
+
+            if (numero == 0 || numero == 1)
+            {
+                resuelto = true;    
+            }
+
+            return resuelto;
+        }
+    }
+}
---
+++ PrimosApp/PrimosApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PrimosApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            do
+            {
+                Console.WriteLine("Escribe un numero y te diré si es primo: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out numero));
+
+            numero = Math.Abs(numero);
+
+            bool esPrimo = PrimosManager.isPrimo(numero);
+
+            Console.WriteLine("El numero {0} es primo es {1}", numero, esPrimo);
+
+            Console.ReadKey();
+        }
+    }
+}
